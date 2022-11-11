7b8d13025c5395930c59e6c332d0df712c2c1be2
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-10 21:08:56 -0800

    serving static files now

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express();
+const PORT = process.env.PORT || 3001
+app.use(cors())
+app.use(express.static(path.resolve(__dirname, '../build/')))
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log(`frontend server listening to port ${PORT}`)
+})
