8e1928ec37b1fa6a266a69ae641db459823db24b
Matt Gallucci <mattgallucci97@gmail.com>
2022-11-13 17:38:54 -0800

    fixed html path issue

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3001
 app.use(cors())
-app.use(express.static(path.resolve(__dirname, '../build/')))
+app.use(express.static(path.resolve(__dirname, './build/')))
 
 app.get('*', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  res.sendFile(path.join(__dirname, './build', 'index.html'))
 })
 
 app.listen(PORT, () => {
